dbb01b9aabf0b0bae45473c70d48d3f97a4340e9
luolin <rhino_luo@163.com>
2019-04-13 23:09:25 +0800

    first commit

---
+++ app/src/main/java/com/rhino/easydevdemo/http/ApiService.java
@@ -0,0 +1,21 @@
+package com.rhino.easydevdemo.http;
+
+
+import com.rhino.easydevdemo.http.result.BaseResult;
+
+import io.reactivex.Observable;
+import retrofit2.http.POST;
+
+
+/**
+ * @author LuoLin
+ * @since Create on 2019/2/19.
+ */
+public interface ApiService {
+
+    String BASE_URL = "http://www.51jiaji.cn:802/crm/";
+
+    @POST("MP/GetVersion")
+    Observable<BaseResult<String>> getVersion();
+
+}
---
+++ app/src/main/java/com/rhino/easydevdemo/http/HttpApi.java
@@ -0,0 +1,23 @@
+package com.rhino.easydevdemo.http;
+
+/**
+ * @author LuoLin
+ * @since Create on 2019/2/19.
+ */
+public class HttpApi extends com.rhino.libretrofit2okhttp3.HttpUtils {
+
+    private static ApiService mApiService = null;
+
+    private static void init() {
+        BASE_URL = ApiService.BASE_URL;
+        mApiService = instance().getService(ApiService.class);
+    }
+
+    public static ApiService api() {
+        if (mApiService == null) {
+            init();
+        }
+        return mApiService;
+    }
+
+}
